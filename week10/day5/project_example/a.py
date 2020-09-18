cc818998ba6fa77c930414c5537efb054b1a8323
gabvaztor <gabvaztor@gmail.com>
2020-09-18 14:02:01 +0200

    CW10D5 after class

--- week10/day5/project_example/a.py
+++ week10/day5/project_example/a.py
@@ -1,2 +1,3 @@
 def funcion_a():
     print("Debo estudiar funciones y en la semana de R voy a atender y practicar mucho porque me he dado cuenta que lo necesito seriamente para pasar entrevistas técnicas.")
+print("FILE ES:\n\n:", __file__) 
---
+++ week10/day5/theory/b.py
@@ -0,0 +1,4 @@
+def function_b():
+    print("b")
+    
+print()
